9820b1bd4f5293c40512829c4a6685b50552180d
b1acksh33p <52150846+b1acksh33p@users.noreply.github.com>
2019-11-28 22:34:26 +0100

    Update new_data_influxdb.py

--- skripts/new_data_influxdb.py
+++ skripts/new_data_influxdb.py
@@ -1,6 +1,38 @@
+import pandas as pd
 from influxdb import InfluxDBClient
+from influxdb import DateFrameClient
 
-database = db
 
-# using Http
-client = InfluxDBClient(database=database)
+
+host = 'localhost'
+port = 8086
+username = 'root'
+password = 'root'
+database = 'demo'
+
+# This part will be used when planning to work with pandas dataframes
+
+# Pandas: Instantinate connection to Database 
+client = DateFrameClient(host = host,
+                         port = port,
+                         username = username,
+                         password = password 
+                         database = database)
+
+json_body
+
+# Create new database
+client.create_database('demo')
+
+#JSON: Instantinate connection to Database
+client = InfluxDBClient (host = host,
+                         port = port,
+                         username = username,
+                         password = password 
+                         database = database)
+
+
+#create a new database
+client.create('database')
+
+
